5dff3125319d19cba00f00f96cf95e7b78dafef5
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2025-01-18 18:09:03 -0600

    Updated the stops to use the temp data

--- src/components/StopSearchModal.tsx
+++ src/components/StopSearchModal.tsx
@@ -10,12 +10,36 @@ import {
   IonLabel,
   IonIcon,
   IonBadge,
+  IonText,
 } from "@ionic/react";
 import { bus, swapHorizontal } from "ionicons/icons";
+import { stops, Stop } from "../misc/stopsData";
 
 import "./StopSearchModal.scss";
 
 const StopSearch: React.FC = () => {
+  const renderStopOptions = () => {
+    const stopOptions = stops.map((stop) => {
+      return (
+        <IonItem button routerLink="/route/blue" key={stop.id}>
+          <IonIcon icon={bus} slot="start" />
+          <IonLabel>
+            <h2>{stop.name}</h2>
+            <p>{stop.routeId}</p>
+            {/* <p>
+              <IonIcon icon={swapHorizontal} /> MEX 2
+            </p>
+            <p>Blvd Lic Manuel Cav...</p> */}
+          </IonLabel>
+          <IonBadge slot="end" color="primary">
+            4 buses
+          </IonBadge>
+        </IonItem>
+      );
+    });
+
+    return <IonList>{stopOptions}</IonList>;
+  };
   return (
     <IonPage id="stop-search-modal">
       <IonHeader>
@@ -24,34 +48,11 @@ const StopSearch: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonList>
-          <IonItem button routerLink="/route/blue">
-            <IonIcon icon={bus} slot="start" />
-            <IonLabel>
-              <h2>Calle Calixto de Ayala</h2>
-              <p>
-                <IonIcon icon={swapHorizontal} /> MEX 2
-              </p>
-              <p>Blvd Lic Manuel Cav...</p>
-            </IonLabel>
-            <IonBadge slot="end" color="primary">
-              4 buses
-            </IonBadge>
-          </IonItem>
-          <IonItem button routerLink="/route/green">
-            <IonIcon icon={bus} slot="start" />
-            <IonLabel>
-              <h2>Calle Diagonal Cuauhtemoc</h2>
-              <p>
-                <IonIcon icon={swapHorizontal} /> Calle Sexta
-              </p>
-              <p>Calle Jose Maria Mo...</p>
-            </IonLabel>
-            <IonBadge slot="end" color="primary">
-              2 buses
-            </IonBadge>
-          </IonItem>
-        </IonList>
+        <div className="divider-container">
+          <div className="divider-line"></div>
+          <IonText>Paradas</IonText>
+        </div>
+        {renderStopOptions()}
       </IonContent>
     </IonPage>
   );
